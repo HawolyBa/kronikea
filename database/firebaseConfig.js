0b6ab8703abcc333384ef275196050c32dbbc277
Hawoly Ba <hawolyba@gmail.com>
2023-07-21 09:23:02 +0200

    firestore intialization

--- database/firebaseConfig.js
+++ database/firebaseConfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, initializeFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { GoogleAuthProvider } from "firebase/auth";
@@ -28,7 +28,9 @@ const devConfig = {
 const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
 
 const app = initializeApp(config);
-const db = getFirestore(app);
+const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+})
 const auth = getAuth(app)
 const storage = getStorage(app)
 const provider = new GoogleAuthProvider();
