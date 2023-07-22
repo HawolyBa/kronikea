4c572d63ee6447126680a3929a111ca3130fd400
Hawoly Ba <hawolyba@gmail.com>
2023-07-22 13:28:32 +0200

    clear variables

--- database/firebaseConfig.js
+++ database/firebaseConfig.js
@@ -6,13 +6,13 @@ import { GoogleAuthProvider } from "firebase/auth";
 
 
 const prodConfig = {
-  apiKey: `${process.env.NEXT_PUBLIC_FIREBASE_API_KEY}`,
-  authDomain: `${process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}`,
-  databaseURL: `${process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL}`,
-  projectId: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}`,
-  storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.NEXT_PUBLIC_FIREBASE_APP_ID}`,
+  apiKey: "AIzaSyBI9ITjYUCZ6YedemNiY9EXmKMLEx6yCys",
+  authDomain: "story-center.firebaseapp.com",
+  databaseURL: "https://story-center.firebaseio.com",
+  projectId: "story-center",
+  storageBucket: "story-center.appspot.com",
+  messagingSenderId: "1095246518275",
+  appId: "1:1095246518275:web:de42acc0aab573506e5636"
 };
 
 const devConfig = {
