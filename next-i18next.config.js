867db8f5e19df2a31dfd081c3beb1f52c3a860a5
Hawoly Ba <hawolyba@gmail.com>
2023-07-23 14:36:00 +0200

    change i18 config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,10 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     locales: ['en', 'fr'],
+    fallbackLng: ['en', 'fr'],
     defaultLocale: 'en',
-  }
+  },
+  localePath: typeof window === 'undefined' ? path.resolve('public', 'locales') : '/public/locales',
 }
