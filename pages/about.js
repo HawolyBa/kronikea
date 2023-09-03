8f6c8b581d378c77f97fe797858010c2577102b0
Hawoly Ba <hawolyba@gmail.com>
2023-09-03 21:40:35 +0200

    contact page translation

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="p-4 w-full">
+      <div className='bg-white dark:bg-zinc-900 rounded-xl max-w-screen-lg py-8 md:px-12 px-4 mx-auto shadow-lg mb-12'>
+        <h2>À propos de nous</h2>
+      </div>
+    </div>
   )
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["form", "contact", "common", "legals"]))
+    }
+  }
 }
 
 export default About
--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
 import { useAuth } from '../database/auth'
 import { Form, Input, Alert } from 'antd'
 
 import Button from '../components/common/Button'
-import Link from 'next/link'
 import { useSendMessageMutation } from '../database/reducers/profile'
 
 const Contact = () => {
@@ -15,18 +15,21 @@ const Contact = () => {
   const auth = useAuth()
   const [send] = useSendMessageMutation()
   const [message, setMessage] = React.useState('')
+  const [empty, setEmpty] = React.useState(true)
 
   const submit = async values => {
-    await send({
-      userId: auth?.user?.uid,
-      username: auth?.user?.username,
-      message: values.body
-    }).unwrap()
-      .then((res) => {
-        setMessage(res)
-        form.resetFields()
-      })
-      .catch(err => console.log(err))
+    if (values.body) {
+      await send({
+        userId: auth?.user?.uid,
+        username: auth?.user?.username,
+        message: values.body
+      }).unwrap()
+        .then((res) => {
+          setMessage(res)
+          form.resetFields()
+        })
+        .catch(err => console.log(err))
+    }
   }
 
   const resetMessage = () => {
@@ -40,37 +43,40 @@ const Contact = () => {
       </Head>
       <div className="contact p-4 w-full">
         <div className='bg-white dark:bg-zinc-900 rounded-xl max-w-screen-lg py-8 md:px-12 px-4 mx-auto shadow-lg'>
-          <h2 className='text-center text-2xl uppercase mb-8'>Contact Us</h2>
-          <p className="mb-4">Do you have questions or comments for Kronikea? We would love to hear from you! Please fill out the form below and we will get back to you as soon as possible.</p>
+          <h2 className='text-center text-2xl uppercase mb-8'>{t('legals:contact-us')}</h2>
+          <p className="mb-4">{t('legals:contact-us-intro')}</p>
           <div className='my-8'>
             {
               !auth.isLoading && auth.user ?
                 <ul className="mb-4">
-                  <li>Your ID: {auth?.user?.uid}</li>
-                  <li>Your username: {auth?.user?.username}</li>
+                  <li>{t('legals:your-id')}: {auth?.user?.uid}</li>
+                  <li>{t('legals:your-username')}: {auth?.user?.username}</li>
                 </ul> :
-                <Link href="/auth" className="underline block mb-4">Please login to fill the form</Link>
+                <Link href="/auth" className="underline block mb-4">{t('legals:contact-us-login')}</Link>
             }
-            <p className='mb-4'>Alternatively, you can reach us via email at <b>contact@kronikea.com </b></p>
+            <p className='mb-4'>{t('legals:contact-us-alternative')} <b>contact@kronikea.com </b></p>
             <Form
               form={form}
               layout="vertical"
               initialValues={{ body: "" }}
-              onValuesChange={resetMessage}
+              onValuesChange={v => {
+                setEmpty(v.body ? false : true)
+                resetMessage()
+              }}
               onFinish={submit}
             >
               <Form.Item name="body" >
-                <Input.TextArea placeholder="How can we help you ?" rows={12}></Input.TextArea>
+                <Input.TextArea placeholder={t('legals:contact-us-placeholder')} rows={12}></Input.TextArea>
               </Form.Item>
               <div className='flex items-center justify-center'>
-                <Button disabled={!auth.user} type="submit" color='bg-primary'>Send</Button>
+                <Button disabled={!auth.user || empty} type="submit" color='bg-primary'>{t('legals:send-button')}</Button>
               </div>
             </Form>
             <div className='my-3'>
               {message && <Alert message={message} type="success" />}
             </div>
           </div>
-          <p className="mb-4">Follow us on social media for the latest news, product updates, and promotions:</p>
+          <p className="mb-4">{t('legals:follow-us')}:</p>
           <ul>
             <li>
               Facebook: @kronikea
@@ -91,7 +97,7 @@ const Contact = () => {
 export async function getStaticProps({ locale }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ["form", "contact", "common"]))
+      ...(await serverSideTranslations(locale, ["form", "contact", "common", "legals"]))
     }
   }
 }
